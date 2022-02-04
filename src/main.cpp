497b428e868b141c367e21dd0e825f9fef43114e
Wracce <wracce@gmail.com>
2022-02-04 13:49:39 +0400

    Add unit 2 exercises 4,5

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@ int main()
 {
 	//unit2::ex1();
 	//unit2::ex2();
-
-	unit2::ex3p1();
-	unit2::ex3p1();
-	unit2::ex3p2();
-	unit2::ex3p2();
-
+	//unit2::ex3p1();
+	//unit2::ex3p1();
+	//unit2::ex3p2();
+	//unit2::ex3p2();
+	//unit2::ex4();
+	int t;
+	std::cout << "Enter a Celsius value: "; std::cin >> t;
+	std::cout << t << " degrees Celsius is "<< unit2::ex5(t) <<" degrees Fahrenheit\n";
 	system("pause");
 	return 0;
 }
--- src/unit2.h
+++ src/unit2.h
@@ -26,4 +26,16 @@ namespace unit2
 	{
 		std::cout << "See how they run" << std::endl;
 	}
+
+	void ex4()
+	{
+		int age;
+		std::cout << "Enter your age: "; std::cin >> age;
+		std::cout << "Your age in months is: " << 12 * age << std::endl;
+	}
+
+	double ex5(int t)
+	{
+		return 1.8 * t + 32;
+	}
 }
